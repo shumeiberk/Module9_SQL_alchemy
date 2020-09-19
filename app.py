bae9603f78cb6046bbd44bbac969d90b4626d301
Ou <ous1@nala.roche.com>
2020-09-19 11:35:44 -0700

    flask update

--- app.py
+++ app.py
@@ -1,7 +1,50 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+######################################
+# Database setup
+######################################
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session from python to the DB
+session = Session(engine)
+
+
+######################################
+# Flask setup
+######################################
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello world'
+def welcome():
+	return(
+      '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+if __name__ == '__main__':
+    app.run()
